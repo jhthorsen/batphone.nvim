50a106179b967c6efc0a8f291f9d91873419e3b4
Jan Henning Thorsen <jan.henning@thorsen.pm>
2025-02-25 23:19:00 +0900

    Add find_package_files and find_parent_files

--- lua/batphone/keymaps/plugins.lua
+++ lua/batphone/keymaps/plugins.lua
@@ -1,3 +1,36 @@
+local find_package_files = function()
+  require("telescope.builtin").find_files({
+    cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+  })
+end
+
+local find_parent_files_cache = {}
+local find_parent_files = function(opts)
+  opts = opts or {}
+
+  local parent_dir = vim.fn.fnamemodify(vim.uv.cwd(), ":h")
+  local dynamic_finder = {
+    entry_maker = require("telescope.make_entry").gen_from_file({ cwd = parent_dir }),
+    fn = function()
+     local results = find_parent_files_cache[parent_dir]
+     if not results then
+       results = vim.fn.systemlist("rg --files " .. parent_dir)
+       find_parent_files_cache[parent_dir] = results
+     end
+
+     return results
+    end,
+  }
+
+  require("telescope.pickers").new(opts, {
+    prompt_title = "Parent files",
+    debounce = 200,
+    finder = require("telescope.finders").new_dynamic(dynamic_finder),
+    previewer = nil,
+    sorter = require("telescope.config").values.file_sorter(opts),
+  }):find()
+end
+
 local format_code = function()
   require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
 end
@@ -49,11 +82,13 @@ return {
     { "<c-p>", "<cmd>Telescope git_files<cr>", desc = "Find Files (git-files)" },
     { "<leader>b", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Switch Buffer" },
     { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-    { "<leader>sp", "<cmd>Telescope spell_suggest<cr>", desc = "Quickfix List" },
+    { "<leader>sp", "<cmd>Telescope spell_suggest<cr>", desc = "Spell suggestions" },
     { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find all files" },
-    { "<leader>fG", "<cmd>Telescope find_files<cr>", desc = "Find git files" },
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+    { "<leader>fG", "<cmd>Telescope git_files<cr>", desc = "Find git files" },
+    { "<leader>fP", find_package_files, desc = "Find package files" },
+    { "<leader>fp", find_parent_files, desc = "Find parent files" },
     { "<leader>gc", "<cmd>Telescope git_commits<CR>", desc = "Commits" },
     { "<leader>gs", "<cmd>Telescope git_status<CR>", desc = "Status" },
     { "<leader>s\"", "<cmd>Telescope registers<cr>", desc = "Registers" },
@@ -61,8 +96,8 @@ return {
     { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
     { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
     { "<leader>sC", "<cmd>Telescope commands<cr>", desc = "Commands" },
-    { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
-    { "<leader>sD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
+    { "<leader>db", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document Diagnostics" },
+    { "<leader>da", "<cmd>Telescope diagnostics<cr>", desc = "Workspace Diagnostics" },
     { "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Help Pages" },
     { "<leader>sH", "<cmd>Telescope highlights<cr>", desc = "Search Highlight Groups" },
     { "<leader>sj", "<cmd>Telescope jumplist<cr>", desc = "Jumplist" },
