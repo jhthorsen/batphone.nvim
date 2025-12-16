02bdc0142e9891e2f68782cbc9e3daa1f1da3529
Jan Henning Thorsen <jan.henning@thorsenlabs.com>
2025-12-16 15:09:08 +0900

    Bumped codecompanion.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,7 +12,7 @@ vim.pack.add({
   { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("1") },
   { src = "https://github.com/fang2hou/blink-copilot", version = "main" },
   { src = "https://github.com/ravitemer/codecompanion-history.nvim", version = "main" },
-  { src = "https://github.com/olimorris/codecompanion.nvim", version = vim.version.range("17") },
+  { src = "https://github.com/olimorris/codecompanion.nvim", version = vim.version.range("18") },
   { src = "https://github.com/mrcjkb/rustaceanvim", version = vim.version.range("6") },
   { src = "https://github.com/stevearc/quicker.nvim", version = "master" },
   { src = "https://github.com/uga-rosa/ccc.nvim", version = "main" },
